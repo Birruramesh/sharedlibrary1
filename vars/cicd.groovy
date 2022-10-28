33fea2d30f5607f91ef15016e7f1cf1cd3d5aeb7
Birruramesh <114664459+Birruramesh@users.noreply.github.com>
2022-10-28 08:50:38 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,11 +2,3 @@ def newGit(repo)
 {
   git "${repo}"
 }
-def newMaven()
-{
-  sh 'mvn package'
-}
-newDeploy(jobname,ip,context)
-{
-  sh 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:${context}.war'
-}
